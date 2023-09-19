cfaa49b05f8de2d505bed4ec45deffa4c6ed06b9
Luelen Cavalheiro <luelencavalheiro@hotmail.com>
2023-09-19 17:40:15 -0300

    adição do banco de dados da Iara  e install token

---
+++ database.sql
@@ -0,0 +1,23 @@
+create database dindin;
+
+create table usuarios (
+	id serial primary key,
+  nome text not null,
+  email text not null unique,
+  senha text not null
+);
+
+create table categorias (
+    id serial primary key,
+  descricao text not null
+);
+
+create table transacoes (
+    id serial primary key,
+  descricao text not null,
+  valor integer not null,
+  data timestamp default now(),
+  categoria_id integer references categorias(id),
+  usuario_id integer references usuarios(id),
+  tipo text not null
+);
