c88c74a3ae3f03944d8a577d62fd525f8e60e078
Luelen Cavalheiro <luelencavalheiro@hotmail.com>
2023-09-19 16:12:00 -0300

    adicionando alguns arquivos e pastas

--- src/index.js
+++ src/index.js
@@ -1,41 +1,13 @@
 const express = require('express');
+const rotas = require('./rotas');
+
 
 const app = express();
 
-
-
-// cadastrar usuario
-app.post('/usuario', (req, res) => { });
-
-// fazer login
-app.post('/login', (req, res) => { });
-
-// detalhar perfil do usuario
-app.get('/usuario', (req, res) => { });
-
-// editar perfil do usuario
-app.put('/usuario', (req, res) => { });
-
-// listar categorias
-app.get('/categoria', (req, res) => { });
-
-// listar transações
-app.get('/transacao', (req, res) => { });
-
-// detalhar transação
-app.get('/transacao/:id', (req, res) => { });
-
-// cadastrar transação
-app.post('/transacao', (req, res) => { });
-
-// editar transação
-app.put('/transacao/:id', (req, res) => { });
-
-// remover transação
-app.delete('/transacao/:id', (req, res) => { });
-
-// obter extrato das transações
-app.get('/transacao/extrato', (req, res) => { });
+app.use(express.json());
+app.use(rotas)
 
 
 app.listen(3000);
+
+
---
+++ src/rotas.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const rotas = express();
+
+
+// cadastrar usuario
+rotas.post('/usuario', (req, res) => { });
+
+// fazer login
+rotas.post('/login', (req, res) => { });
+
+// detalhar perfil do usuario
+rotas.get('/usuario', (req, res) => { });
+
+// editar perfil do usuario
+rotas.put('/usuario', (req, res) => { });
+
+// listar categorias
+rotas.get('/categoria', (req, res) => { });
+
+// listar transações
+rotas.get('/transacao', (req, res) => { });
+
+// detalhar transação
+rotas.get('/transacao/:id', (req, res) => { });
+
+// cadastrar transação
+rotas.post('/transacao', (req, res) => { });
+
+// editar transação
+rotas.put('/transacao/:id', (req, res) => { });
+
+// remover transação
+rotas.delete('/transacao/:id', (req, res) => { });
+
+// obter extrato das transações
+rotas.get('/transacao/extrato', (req, res) => { });
+
+
+module.exports = rotas
+
