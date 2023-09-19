c7c7f9b736173fa3fac4992458d25b5781021147
Iara Esteves Flaminio <iaraflaminio@outlook.com>
2023-09-19 20:08:51 -0300

    inserção das categorias - tabela categorias

--- database.sql
+++ database.sql
@@ -21,3 +21,23 @@ create table transacoes (
   usuario_id integer references usuarios(id),
   tipo text not null
 );
+
+insert into categorias (descricao)
+values 
+('Alimentação'),
+('Assinaturas e Serviços'),
+('Casa'),
+('Mercado'),
+('Cuidados Pessoais'),
+('Educação'),
+('Família'),
+('Lazer'),
+('Pets'),
+('Presentes'),
+('Roupas'),
+('Saúde'),
+('Transporte'),
+('Salário'),
+('Vendas'),
+('Outras receitas'),
+('Outras despesas')
