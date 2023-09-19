9b48397e031a6ad9dd371b59fc4bb9ad2bf15052
Luelen Cavalheiro <luelencavalheiro@hotmail.com>
2023-09-19 17:01:07 -0300

    implementação da rota cad.usuario

--- controladores/usuario.js
+++ controladores/usuario.js
@@ -14,14 +14,20 @@ const cadastrarUsuario = async (req, res) => {
     }
 
     try {
-        const query = 'insert into usuarios (nome, email, senha) values ($1, $2, $3) returning (id, nome, email) '
-        const { rows } = await pool.query(query, [nome, email, senha])
-
-        return res.status(201).json(rows[0])
+        const { rowCount } = await pool.query(' select * from usuarios where email = $1', [email])
+        if (rowCount === 0) {
+            const query = 'insert into usuarios (nome, email, senha) values ($1, $2, $3) returning (id, nome, email) '
+            const { rows } = await pool.query(query, [nome, email, senha])
+            return res.status(201).json(rows[0])
+        }
+        return res.status(400).json({ mensagem: 'Já existe um email igual a esse cadastrado' })
     } catch (error) {
         return res.status(500).json({ mensagem: 'Erro interno do servidor' })
     }
+
+    // FALTA Criptografar a senha antes de persistir no banco de dados
 }
+
 
 
 
